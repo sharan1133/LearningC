3a075ee36b59999bdcfde0df64a0ecb1ccd80bb8
sharan1133 <sharanbala2138@protonmail.com>
2024-03-07 20:04:59 +0530

    07/03/2024

--- JUNK/junk_four.c
+++ JUNK/junk_four.c
@@ -1,9 +1,28 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
-# include "../DOUBLY_LINKED_LIST/doubly_linked_list_h.h"
+# include "../LINKED_LIST/doubly_linked_list_h.h"
 
 
+# define DELETEATPOS( new , num) \
+	DeleteAtPos(&new , num); \
+	Display(new);
+
+# define INSERTLAST(new , value) \
+	InsertAtLast(&new , value); \
+	Display(new);
+
+
+# define INSERTATPOS(new , value , pos) \
+	InsertAtPos(&new , value , pos); \
+	Display(new);
+
+# define INSERTFRONT(new , value) \
+	InsertAtFront(&new , value); \
+	Display(new);
+
+
+# define COUNT GetCount(new)
 
 void get_original_number_back(int number , int k , int truncation)
 {
@@ -17,20 +36,14 @@ void get_original_number_back(int number , int k , int truncation)
 int main(void)
 
 {
+	
 
 	node *new = NULL;
-	InsertAtLast(&new , 10);
-	InsertAtLast(&new , 20);
-	InsertAtLast(&new , 30);
-	InsertAtLast(&new , 40);
-	InsertAtLast(&new , 50);
-	Display(new);
-	InsertAtPos(&new , 1 , GetCount(new));
-	Display(new);
-	//InsertAtPos(&new , 1 , 2);
-	//Display(new);
-	
-	//get_original_number_back(86523 , 5 , 0);
-	
+	INSERTFRONT(new , 10);
+	INSERTFRONT(new , 20);
+	INSERTLAST(new , 50);
+	INSERTLAST(new , 60);
+	INSERTATPOS(new , 7620 , COUNT);
+	printf(" number of elements : %d \n " , COUNT);
 	return 0;
 }
