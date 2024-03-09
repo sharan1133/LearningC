aaba665270f869548dc4cf402de4dbefdbffd569
sharan1133 <sharanbala2138@protonmail.com>
2024-03-09 19:43:24 +0530

    09/03/2024

---
+++ JUNK/junk_five.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+
+typedef struct Block
+{
+		
+}
+
+
+typedef struct Node 
+{
+	struct Node *next;
+	int data;
+}node;
+
+
+int main(void)
+{
+	return 0;
+}
