61a6140e648d45d424106d7dd34110a6e22b8d98
sharan1133 <sharanbala2138@protonmail.com>
2024-05-08 21:36:05 +0530

    08/05/2024

---
+++ pointers.c
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <stdarg.h>
+
+int main(void)
+{
+	int* ptr = (int*)malloc(sizeof(int) * 11);
+	int** ptr2 = (int**)calloc(5 , sizeof(int*));
+	/*for(int i = 0 ; i < 11 ; i++)
+		*(ptr + i) = 100 + i;
+	for(int i = 0 ; i < 10 ; i++)
+		printf(" %d  " , *(ptr+i));*/
+
+	for(int i = 0 ; i < 5 ; i++)
+	{
+		*(ptr2 + i) = (int*)malloc(sizeof(int) * 3);
+		for(int j = 0 ; j < 3 ; j++)
+		{
+			*(*(ptr2+i) + j) = 10;	
+		}
+	}
+
+
+	for(int i = 0 ; i < 5 ; i++)
+	{
+		for(int j = 0 ; j < 3 ; j++)
+		{
+			//*(*(ptr2+i) + j) = 10;	
+			printf(" %d    " , *(*(ptr2+i) + j));  
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+	return 0;
+}
